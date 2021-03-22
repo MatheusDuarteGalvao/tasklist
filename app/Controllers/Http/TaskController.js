74d6bdceda53dd907f9c3070afb4552712a434e4
Matheus Duarte <matheus.duarte@uaufi.com>
2021-03-22 14:20:18 -0300

    Add views

--- app/Controllers/Http/TaskController.js
+++ app/Controllers/Http/TaskController.js
@@ -1,6 +1,17 @@
-'use strict'
+"use strict";
 
 class TaskController {
+  index({ view }) {
+    const tasks = [
+      { title: 'Task one', body: 'This is task one' },
+      { title: 'Task two', body: 'This is task two' }
+    ]
+
+    return view.render('task', {
+      title: 'Yours tasks',
+      tasks: tasks
+    })
+  }
 }
 
-module.exports = TaskController
+module.exports = TaskController;
